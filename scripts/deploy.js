5e7666f6bf79192714f9a0123d2b633c4189de09
rsotomay <138070004+rsotomay@users.noreply.github.com>
2023-09-06 22:48:50 -0500

    Adds react app

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,18 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const NAME = "GASton Token";
+  const SYMBOL = "GSNT";
+  const MAX_SUPPLY = "1000000";
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // Deploy Token
+  const Token = await hre.ethers.deployContract("Token", [
+    NAME,
+    SYMBOL,
+    MAX_SUPPLY,
+  ]);
+  await Token.waitForDeployment();
+  console.log(`Token Deployed to: ${Token.target}\n`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
